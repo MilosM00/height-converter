e80dba0fd596da97e91f57024d96bdc159b69da6
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-20 09:52:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+const buttonChangeOne = document.querySelector(`.change`);
+const buttonConvert = document.querySelector(`.convert`);
+const inputMeter = document.querySelector(`.m`);
+const inputCentimeter = document.querySelector(`.cm`);
+const inputFeet = document.querySelector(`.ft`);
+const inputInch = document.querySelector(`.in`);
+const inputMeterTwo = document.querySelector(`.m-2`);
+const inputCentimeterTwo = document.querySelector(`.cm-2`);
+const inputFeetTwo = document.querySelector(`.ft-2`);
+const inputInchTwo = document.querySelector(`.in-2`);
+const overlayOne = document.querySelector(`.overlay-1`);
+const overlayTwo = document.querySelector(`.overlay-2`);
+
+
+let isChange = true;
+
+overlayOne.classList.remove(`hidden-1`);
+
+buttonChangeOne.addEventListener(`click`, () =>{
+    if(isChange === true){
+        overlayTwo.classList.remove(`hidden-2`);
+        overlayOne.classList.add(`hidden-1`);
+
+        inputMeter.value = ``;
+        inputCentimeter.value = ``;
+        inputFeet.value = ``;
+        inputInch.value = ``;
+    }
+    
+    else if(isChange === false){
+        overlayTwo.classList.add(`hidden-2`);
+        overlayOne.classList.remove(`hidden-1`);
+
+        inputFeetTwo.value = ``;
+        inputInchTwo.value = ``;
+        inputMeterTwo.value = ``;
+        inputCentimeterTwo.value = ``;
+    }
+
+    isChange = !isChange;
+});
